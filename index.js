eb073c691b702f583dd9f61b44d9492d711d311e
x1289 <daniel.pschibila@googlemail.com>
2022-06-02 19:24:19 +0200

    [Feature] Allow cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
-const port = 3000
+const port = 8080
+
+app.use(cors());
 
 const defaultValues = require('./default.json');
 
-app.get('/', (req, res) => {
+app.get('/', cors(), (req, res, next) => {
   res.json(defaultValues);
 })
 
